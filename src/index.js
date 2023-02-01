48aa6c97440bf2b3d45ee53e06a8de9515726335
Prerit <preritmishra1002@gmail.com>
2023-02-01 22:20:10 +0530

    Moved to es modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const connect = require('./config/database');
+import express from 'express';
+import {connect} from './config/database.js';
 
 const app = express();
+
+//import HashtagRepository from './repository/hashtag-repository.js';
+import service from './services/tweet-service.js';
 
 app.listen(3000, async () => {
     console.log('Server Started');
     await connect();
     console.log('Mongo db connected');
+    let ser = new service();
+    await ser.create({
+        content: 'done with #refactor ?'
+    });
 });
