21d6988db8701a16384d7ff1ad2822c052bf83c7
Prerit <preritmishra1002@gmail.com>
2023-02-04 12:15:15 +0530

    Added get tweet with nested comment api

--- src/models/comment.js
+++ src/models/comment.js
@@ -14,11 +14,17 @@ const commentSchema = new mongoose.Schema({
         required: true,
         enum: ['Tweet', 'Comment']
     },
-    comentable: {
+    commentable: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         refPath: 'onModel'
-    }
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 }, {timestamps: true});
 
 const Comment = mongoose.model('Comment', commentSchema);
