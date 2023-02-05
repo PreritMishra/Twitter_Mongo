91572696c61cf3b49c4902d83f8de87032edd4cc
Prerit <preritmishra1002@gmail.com>
2023-02-05 13:34:52 +0530

    Added signup with encrypted password and added passport logic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import passport from 'passport';
 
 import {connect} from './config/database.js';
 
+import { passportAuth } from './config/jwt-middleware.js';
+
 import apiRoutes from './routes/index.js';
 
-import {UserRepository, TweetRepository} from './repository/index.js';
 
-import LikeService from './services/like-service.js';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(passport.initialize());
+passportAuth(passport);
 
 app.use('/api', apiRoutes);
 
@@ -20,11 +24,5 @@ app.listen(3000, async () => {
     await connect();
     console.log('Mongo db connected');
 
-    // const userRepo = new UserRepository();
-    // const tweetRepo = new TweetRepository();
-    // const tweets = await tweetRepo.getAll(0,10);
-    // const users = await userRepo.getAll();
-    // const likeService = new LikeService();
-    // likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
 
 });
